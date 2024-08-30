2618603c48cc29a5b1f394e34296460aa22d371a
jswapneswar <104552616+jswapneswar@users.noreply.github.com>
2024-08-30 13:29:42 +0530

    Add files via upload

---
+++ Mf3/components/componentsF3/Component1.js
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "./../../assets/logo.svg";
+import about from "./../../assets/about.jpeg";
+import recipe_1 from "./../../assets/recipes/recipe-1.jpeg";
+import recipe_2 from "./../../assets/recipes/recipe-2.jpeg";
+import recipe_3 from "./../../assets/recipes/recipe-3.jpeg";
+import hugeImage from "./../../assets/SampleJPGImage_30mbmb.jpg";
+
+export default function Component1() {
+  return <p>hhh</p>;
+}
